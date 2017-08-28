8508ba8d73180369ddb52280dca08e61d1e0d2f3
Neeraj Yadav <ny29599@gmail.com>
2017-08-28 12:01:28 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>test</title>
+    <style type="text/css">
+        .scroll {
+   width: 200px;
+   height: 400px;
+    background: red;
+   overflow: scroll;
+}
+.scroll::-webkit-scrollbar {
+    width: 12px;
+}
+
+.scroll::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+    border-radius: 10px;
+}
+
+.scroll::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
+}
+    </style>
+</head>
+<body>
+    <div class="scroll">csdsdkfskfkfskfnk</div>
+</body>
+</html>
---
+++ user_closed.php
@@ -0,0 +1,10 @@
+<?php
+    include("includes/header.php");
+?>
+
+<div class="main_coloumn coloumn" id="main_coloumn">
+    <h4>User Closed</h4>
+    
+    This account is closed.
+    <a href="index.php"> Click here to go back.</a>
+</div>
